57a37c23dc5abe7e06fb07f129b53c6fd83c2917
Wealth Iduwe <wealthiduwe@gmail.com>
2021-04-16 06:55:13 +0100

    cleaner js syntax using classList toggle

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ let display = document.getElementById("uls");
 
 document.addEventListener("DOMContentLoaded", () => {
   icon.onclick = () => {
-    if (display.style.left == "100%") {
-      display.style.left = "0px";
-    } else {
-      display.style.left = "100%";
-    }
+    display.classList.toggle("responsive")
   };
 });
